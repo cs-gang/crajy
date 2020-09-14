d35567e0f0be996b1b535b70c9a000ff1d676063
Unknown <moonlight.shriram@gmail.com>
2020-09-14 21:10:27 +0400

    Made the tictactoe starting layout

--- cogs/games.py
+++ cogs/games.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import tictactoe
 import random
+import datetime
 import asyncpg
 
 class games(commands.Cog):
@@ -15,15 +16,32 @@ class games(commands.Cog):
                       usage="<person>")
 
     async def ttt(self, ctx, opponent: discord.Member = None):
-        if opponent == ctx.message.author:
-            return await ctx.send("you moron, trying to play with yourself.")
+        #if opponent == ctx.message.author:
+            #return await ctx.send("you moron, trying to play with yourself.")
         out = ""
         board = tictactoe.initial_state()
         start = random.choice([tictactoe.X, tictactoe.O])
         players = {tictactoe.X: ctx.author, tictactoe.O: opponent}
 
-        original_message = await ctx.send(
-            content=f"**New Game of Tictactoe** \n {ctx.author.mention} X vs {opponent.mention} O \n {out} \n {start} starts! (make a move for the board to appear)")
+        original_message_embed = discord.Embed(title="TicTacToe Game!",
+                                               description=f"{ctx.author.mention} has challenged {opponent.mention}!",
+                                               timestamp=datetime.datetime.utcnow())
+        original_message_embed.set_thumbnail(url=r"https://media.discordapp.net/attachments/749227065512820736/755093446263439540/download.png")
+        original_message_embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        original_message_embed.set_footer(text=opponent.display_name, icon_url=opponent.avatar_url)
+        original_message = await ctx.send(embed=original_message_embed)
+
+        top_row_message = await ctx.send(content="*top row*")
+        for i in ["↖", "⬆", "↗"]:
+            await top_row_message.add_reaction(i)
+
+        middle_row_message = await ctx.send(content="*middle row*")
+        for i in ["⬅", "⏺", "➡"]:
+            await middle_row_message.add_reaction(i)
+
+        bottom_row_message = await ctx.send(content="*bottom row*")
+        for i in["↙", "⬇", "↘"]:
+            await bottom_row_message.add_reaction(i)
 
         def player1_check(m):
             if m.author == ctx.message.author and len(m.content.split()) == 2:
@@ -41,33 +59,20 @@ class games(commands.Cog):
                     return False
                 return True
 
-        while not tictactoe.terminal(board):
+        '''while not tictactoe.terminal(board):
             if tictactoe.player(board, start) == tictactoe.X:
-                try:
-                    reply = await self.bot.wait_for('message', check=player1_check)
-                    if not tictactoe.valid_action(tuple([int(i) for i in reply.content.split()]), board):
-                        raise IndexError
-                    coords = [int(i) for i in reply.content.split()]
-                    board[coords[0]][coords[1]] = tictactoe.X
-                except IndexError:
-                    await ctx.send(f"{tictactoe.player(board, start)}, you've tried an invalid move")
+
+
 
                 out = tictactoe.board_converter(board)
                 await original_message.edit(content=out)
             elif tictactoe.player(board, start) == tictactoe.O:
-                try:
-                    reply = await self.bot.wait_for('message', check=player2_check)
-                    if not tictactoe.valid_action(tuple([int(i) for i in reply.content.split()]), board):
-                        raise IndexError
-                    coords = [int(i) for i in reply.content.split()]
-                    board[coords[0]][coords[1]] = tictactoe.O
-                except IndexError:
-                    await ctx.send(f"{tictactoe.player(board, start)}, you've tried an invalid move")
+
                 out = tictactoe.board_converter(board)
                 await original_message.edit(content=out)
         else:
             win_person = players[tictactoe.winner(board)]
-            await ctx.send(f"game over, winner is {tictactoe.winner(board)}")
+            await ctx.send(f"game over, winner is {tictactoe.winner(board)}")'''
 
     pass
 
