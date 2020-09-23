3b01b01ef3c3bfdc63f9440281a8e5aa9027c6be
Unknown <moonlight.shriram@gmail.com>
2020-09-23 15:04:31 +0400

    Finished game loop, need to fix bugs

--- cogs/games.py
+++ cogs/games.py
@@ -20,15 +20,17 @@ class games(commands.Cog):
             #return await ctx.send("you moron, trying to play with yourself.")
         out = ""
         board = tictactoe.initial_state()
-        start = random.choice([tictactoe.X, tictactoe.O])
+        player = random.choice([tictactoe.X, tictactoe.O])
+        next_player = tictactoe.X if player == tictactoe.O else tictactoe.O
         players = {tuple(tictactoe.X): ctx.author, tuple(tictactoe.O): opponent} #check cause of error because tuple() is unnecesary
 
         main_message_embed = discord.Embed(title="TicTacToe Game!",
-                                            description=f"{ctx.author.mention} has challenged {opponent.mention}!\n {players[tuple(start)]} makes the first move.",
+                                            description=f"{ctx.author.mention} has challenged {opponent.mention}!\n {players[tuple(player)]} makes the first move.",
                                             timestamp=datetime.datetime.utcnow())
         main_message_embed.set_thumbnail(url=r"https://media.discordapp.net/attachments/749227065512820736/755093446263439540/download.png")
         main_message_embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         main_message_embed.set_footer(text=opponent.display_name, icon_url=opponent.avatar_url)
+        main_message_embed.color = discord.Color.blue()
         main_message = await ctx.send(embed=main_message_embed)
         main_message_embed.set_thumbnail(url=discord.Embed.Empty)
 
@@ -45,19 +47,26 @@ class games(commands.Cog):
             await bottom_row_message.add_reaction(i)
 
         def player_check(reaction, user):
-            print(str(reaction.emoji))
-            if str(reaction.emoji) in ["↖", "⬆", "↗", "⬅", "⏺", "➡", "↙", "⬇", "↘"] and user == players[start]:
+            if str(reaction.emoji) in ["↖", "⬆", "↗", "⬅", "⏺", "➡", "↙", "⬇", "↘"] and user == players[player]:
                 return True
+
             return False
 
         while not tictactoe.terminal(board):
             reaction, _ = await self.bot.wait_for("reaction_add", check=player_check)
+            message_of_reaction = reaction.message
 
             if reaction:
-                print(reaction)
-                main_message_embed = tictactoe.update_board(main_message_embed, reaction.emoji, start)
-                print(main_message_embed.description)
+                main_message_embed.color = discord.Color.red()
+                await message_of_reaction.clear_reaction(reaction.emoji)
+                main_message_embed = tictactoe.update_board(main_message_embed, reaction.emoji, player)
+                player, next_player = next_player, player
+                main_message_embed.description += f"\n{players[player]}'s turn"  # maybe move to better place
                 await main_message.edit(embed=main_message_embed)
+
+        main_message_embed.description = f"{players[next_player]} destroyed {players[player]}!\n Good Game!"
+        main_message_embed.color = discord.Color.green()
+        await main_message.edit(embed=main_message_embed)
 
 
 
